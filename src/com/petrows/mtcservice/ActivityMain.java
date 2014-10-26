544596f0fa05b0b95c6579322623e98a3323acf2
Peter Petrovich <petro@petro.ws>
2014-10-26 18:08:00 +0400

    Dependency fixup

--- src/com/petrows/mtcservice/ActivityMain.java
+++ src/com/petrows/mtcservice/ActivityMain.java
@@ -2,8 +2,11 @@ package com.petrows.mtcservice;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class ActivityMain extends Activity {
 	final static String TAG = "MTCService.ActivityMain";
@@ -11,6 +14,17 @@ public class ActivityMain extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		setContentView(R.layout.activity_main);
+		
+		TextView txtVersion = (TextView)findViewById(R.id.txtVersion);
+		String version = "?";
+		try {
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			version = pInfo.versionName;
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		txtVersion.setText("Version: " + version);
 
 		if (Settings.get(this).getServiceEnable() && !ServiceMain.isRunning)
 		{
