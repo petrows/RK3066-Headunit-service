16d94625456df6030dbe320e6989d88162c2297d
Peter Petrovich <petro@petro.ws>
2015-03-31 00:03:35 +0300

    app prefs

--- app/src/main/java/com/petrows/mtcservice/SettingsActivity.java
+++ app/src/main/java/com/petrows/mtcservice/SettingsActivity.java
@@ -1,14 +1,26 @@
 package com.petrows.mtcservice;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.util.Log;
+
+import com.petrows.mtcservice.view.IconListPreference;
 
 public class SettingsActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.pref);
+
+
+		IconListPreference zApps = (IconListPreference) getPreferenceManager().findPreference("test.list");
+		// zApps.setIcon(getResources().getDrawable(R.drawable.ic_launcher));
+		CharSequence[] zAppsTitle = {"a", "b", "z"};
+		zApps.setEntryValues(zAppsTitle);
+		zApps.setEntries(zAppsTitle);
+		zApps.setTitle("Asshole");
 	}
 
 	@Override
